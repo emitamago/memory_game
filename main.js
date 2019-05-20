b1483a8f2bc81035f53572d664c54f89f4df1447
Emi Tsukuda <gioramlevi13@gmail.com>
2019-05-20 02:22:59 -0700

    cards shuffle everytime when game ends auto

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// GLOBAL VARIABLES
-
-// all card contents
-
-
-// cards that not flipped
-
 
 // variable to check if first card get clicked
 var firstCardClicked = false;
@@ -15,11 +8,12 @@ var secondCardClicked = false;
 // temporary place folders to hold two cards that clicked
 var firstCard, secondCard;
 
+// setting permanent place holder for cards
+const GAMEAREA = document.querySelector(".game-area")
 
+// start game on load
 
-// start game
-
-
+startGame()
 
 
 //main function 
@@ -45,11 +39,93 @@ var firstCard, secondCard;
     }
       
     
+    
+    
+    
+
+
+// helper methods-----start
+    // creat card's back
+    function creatBack (num){
+        var newImgBack = document.createElement('img');
+        newImgBack.setAttribute('src', `images/${num}.gif`);
+        newImgBack.classList.add('back');
+        return newImgBack;
+    }
+
+    // create card's front
+    function creatFront(){
+        var newImgFront = document.createElement('img');
+        newImgFront.setAttribute('src', 'images/front.jpg');
+        newImgFront.classList.add('front')
+        return newImgFront;
+    }
+
+    // create individual card 
+       function createCard(k){
+        var individualCard = document.createElement('div');
+        var front = creatFront();
+        var back = creatBack(k);  
+        individualCard.classList.add('cell');
+        individualCard.dataset.number = `card${k}`
+        individualCard.dataset.state = 'undone'
+        individualCard.appendChild(back);
+        individualCard.appendChild(front);
+        return individualCard;
+    }
+    
+    // shuffle single array element
+    function shuffle(arr){
+        for (let k = arr.length-1; k>=0; k-- ) {
+            var randomIndex = Math.floor(Math.random()*(k+1)); 
+            var itemAtIndex = arr[randomIndex]; 
+            arr[randomIndex] = arr[k]; 
+            arr[k] = itemAtIndex;
+        }
+        return arr;
+     }
+
+     // shuffling card's numbers
+    function shuffleCardNumber(){
+        var arrayCardsNumber =[] 
+        for(let i=1;i<=8;i++){arrayCardsNumber.push(i)}
+        var arr1 = shuffle(arrayCardsNumber);
+        var arr2 = shuffle(arrayCardsNumber);
+        return arr1.concat(arr2)
+        }
+
+    // adding individual card
+    function addCard (num){
+        var ACARD = createCard(num)
+        GAMEAREA.appendChild(ACARD);
+        
+    }
+    
+    function startGame(){
+    var cardNumbers = shuffleCardNumber()
+    for(let k of cardNumbers){
+        addCard(k);
+     }
+     var cards = document.querySelectorAll(".cell");
+     addAction(cards)
+    }
+    // adding event listner to all cards so that users can click card and flip
+    function addAction(arr){
+        for(let k of arr){
+             k.addEventListener("click", flipped);
+         }
+      }
+
+// helper methods---during
     // checking if the two cards are match by comparing their uniqe data  
     function isMatched(){
         if(firstCard.dataset.number === secondCard.dataset.number){
         //    matched then the cards are stay flipped
             noMoreFlip();
+            var unflippedCards = document.querySelectorAll("[data-state]")
+            if(unflippedCards.length===2){
+            flipAllCards(unflippedCards);
+            }
         }else{
             //  not matched then unflip card
             unflipCards();
@@ -62,11 +138,17 @@ var firstCard, secondCard;
         secondCardClicked = true;
         // unflip the two card by removing flipped style
         //  setting time　to make sure user can click second card. 
-         setTimeout(() => {
+        setTimeout(() => {
             firstCard.classList.remove('flip');
             secondCard.classList.remove('flip');
             resetBoard();
-         }, 1000);
+        }, 1000);
+    }
+
+    // removing listner and lable the card as done
+    function doneCard(card){
+        card.removeEventListener('click', flipped);
+        card.removeAttribute('data-state')
     }
 
     // to make matched card flipped
@@ -74,98 +156,47 @@ var firstCard, secondCard;
         doneCard(firstCard);
         doneCard(secondCard)
         resetBoard();
-        // tracking how many cards remain unflipped
-        var unflippedCards = document.querySelectorAll("[data-state]")
-        // when remaining card === 2
-        if(unflippedCards.length===2){
-            // flip remaining 2cards
-            setTimeout(flipRemaining,500, unflippedCards);
-            // resetting game
-            setTimeout(resetGame, 3000, cards);
-        }
-         
     }
     
+    function flipAllCards(arr){
+        var exixtingCard = document.querySelectorAll('.cell')
+                // flip remaining 2cards
+            setTimeout(flipRemaining,500, arr);
+                // resetting game
+            
+        setTimeout(flipAllBack, 2000, exixtingCard );
+        setTimeout(reCreateCards, 3000, exixtingCard); 
+    }
+   
     // resetting cards clicking state every 2 cards got clicked
     function resetBoard(){
         [firstCardClicked, secondCardClicked] = [false, false];
         [firstCard, secondCard] = [null, null];
     }
-    
-    // adding event listner to all cards so that users can click card and flip
-    function startGame(){
-       for(let k of cards){
-            k.addEventListener("click", flipped);
-        }
-     }
 
-    function resetGame(arr){
+    function flipAllBack(arr){
         // flip back all cards
         for(let k of arr){
             k.classList.remove("flip")
         }
-        // resetting state of cards
-        resetBoard();
-        // adding lisner so user can restart again
-        startGame();
     }
-    
-    // removing listner and lable the card as done
-    function doneCard(card){
-        card.removeEventListener('click', flipped);
-        card.removeAttribute('data-state')
-    }
-    
+
     // flip remain two cards I may be able to get rid of it later...
     function flipRemaining(arr){
         for(let k of arr){
             k.classList.add("flip")
         }
     }
+    
+    function reCreateCards(arr){
+        for(let k of arr){
+            GAMEAREA.removeChild(k)
+        }
+        startGame()
 
-
-
-    const GAMEAREA = document.querySelector(".game-area")
-    var arr = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
-    for(let k of arr){
-        addCard(k);
-     }
-     var cards = document.querySelectorAll(".cell");
-     startGame()
-// adding individual card
-    function addCard (num){
-        var ACARD = createCard(num)
-        GAMEAREA.appendChild(ACARD);
-        
     }
 
-        // creat card's back
-        function creatBack (num){
-            var newImgBack = document.createElement('img');
-            newImgBack.setAttribute('src', `images/${num}.gif`);
-            newImgBack.classList.add('back');
-            return newImgBack;
-        }
-
-        // create card's front
-        function creatFront(){
-            var newImgFront = document.createElement('img');
-            newImgFront.setAttribute('src', 'images/front.jpg');
-            newImgFront.classList.add('front')
-            return newImgFront;
-        }
-
-        // create individual card 
-        function createCard(k){
-            var individualCard = document.createElement('div');
-            var front = creatFront();
-            var back = creatBack(k);  
-            individualCard.classList.add('cell');
-            individualCard.dataset.number = `card${k}`
-            individualCard.dataset.state = 'undone'
-            individualCard.appendChild(back);
-            individualCard.appendChild(front);
-            return individualCard;
-        }
     
     
+        
+ 
